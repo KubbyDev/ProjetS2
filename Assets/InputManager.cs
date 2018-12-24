def10a5910129172a3debe5e2c63ec109dfefe9b
Gabriel <gbrljorge@gmail.com>
2018-12-24 18:29:51 +0100

    Base de mouvements

---
+++ Assets/InputManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class InputManager : NetworkBehaviour
+{
+    private MovementManager movement;
+    private Spell_BasicBullet basicBullet;
+
+    void Start()
+    {
+        movement = GetComponent<MovementManager>();
+        basicBullet = GetComponent<Spell_BasicBullet>();
+    }
+    
+    void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            movement.Jump();
+
+        if (Input.GetKeyDown(KeyCode.A))
+            basicBullet.Fire();
+
+        movement.Move(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")));
+    }
+}
---
+++ Assets/MovementManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+//Cette classe recoit des inputs de PlayerInputManager pour les actions qui font bouger le joueur
+
+public class MovementManager : MonoBehaviour
+{
+	public float jumpStrength = 1000;
+	public float movementSpeed = 5;
+    public int maxJumps = 3;
+	
+	private Rigidbody rb;
+    private int usableJumps;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+        usableJumps = maxJumps;
+    }
+	
+	public void Move(Vector3 input) 
+	{
+		transform.Translate(input * Time.deltaTime * movementSpeed);
+	}
+
+    public void Jump()
+    {
+        if (usableJumps > 0) //Verifier qu'il reste des sauts possibles
+        {
+            rb.AddForce(new Vector3(0, jumpStrength, 0));
+            usableJumps--;
+        }
+    }
+
+	public void AddForce(Vector3 force) 
+	{
+		rb.AddForce(force);
+	}
+}
