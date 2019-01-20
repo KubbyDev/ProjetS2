a441ac55b18e905db6909996b50d05d8b96a41fc
KubbyDev <gbrljorge@gmail.com>
2019-01-20 19:06:15 +0100

    Debut de Balle + Update 2018.3.1f1

---
+++ Assets/Ball/BallTesting.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class BallTesting : NetworkBehaviour
+{
+    [SyncVar] private Vector3 velocity;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+            CmdAddForceBall(Camera.main.transform.forward * 1000);
+    }
+
+    [Command]
+    void CmdAddForceBall(Vector3 force)
+    {
+        GetComponent<Rigidbody>().AddForce(force);
+    }
+}
