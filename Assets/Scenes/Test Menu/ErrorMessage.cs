3193fbcf4bfc4136bde7431248f420d9474195a2
KubbyDev <gbrljorge@gmail.com>
2019-01-30 00:12:10 +0100

    Fondu sur les messages d'erreur

--- Assets/Scenes/Test Menu/ErrorMessage.cs
+++ Assets/Scenes/Test Menu/ErrorMessage.cs
@@ -5,8 +5,9 @@ using UnityEngine.UI;
 
 public class ErrorMessage : MonoBehaviour
 {
-    [SerializeField] private float displayTime = 1;      //Le temps durant lequel l'erreur reste affichee
-    
+    [SerializeField] [Range(0.5f,5)] private float displayTime = 1;      //Le temps durant lequel l'erreur reste affichee
+    [SerializeField] [Range(0, 1)]   private float fadeTime = 0.8f;      //La proportion du temps total prise par le fondu
+
     [SerializeField] private Text errorMessage;
     private float lastUpdate;
 
@@ -16,14 +17,36 @@ public class ErrorMessage : MonoBehaviour
 
     void Update()
     {
-        if(Time.time - lastUpdate > displayTime)
+        //L'avancement de l'animation (de 0 a 1)
+        float animTime = (Time.time - lastUpdate) / displayTime;
+
+        //Fondu de fin: On modifie le channel alpha (transparence) en fonction du temps
+        if(animTime > 1-fadeTime)
+            modifyAlpha(1 - (animTime - 1 + fadeTime) / fadeTime);
+
+        //Disparition du message
+        if (Time.time - lastUpdate > displayTime)
             this.gameObject.SetActive(false);
     }
 
     public void Display(string message)
     {
+        //Reset de la transparence
+        modifyAlpha(1);
+
+        //Mise a jour du texte et du temps de depart
         errorMessage.text = message;
         lastUpdate = Time.time;
+
+        //Affichage du message
         this.gameObject.SetActive(true);
+    }
+
+    //Modifie la transparence de l'image et du texte du message d'erreur
+    private void modifyAlpha(float newAlpha)
+    {
+        errorMessage.color = new Color(errorMessage.color.r, errorMessage.color.g, errorMessage.color.b, newAlpha);
+        Color imageColor = GetComponent<Image>().color;
+        GetComponent<Image>().color = new Color(imageColor.r, imageColor.g, imageColor.b, newAlpha);
     }
 }
