ad7abdf80669ad993c654a1537dd7f64534b736a
Thomas141100 <141100thomas@gmail.com>
2019-02-18 11:25:08 +0100

    Add Pow-Up Back

---
+++ Assets/Pow-Ups/Back.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Back : MonoBehaviour
+{
+    public int team = 1;
+
+    private Transform blue_pos;
+    private Transform orange_pos;
+
+    private bool Player_Has_Back = true;
+
+    public void Start()
+    {
+        if (Player_Has_Back)
+        {
+            blue_pos = GameObject.Find("blue_pos").transform;
+            orange_pos = GameObject.Find("orange_pos").transform;
+            TP_Back();
+            Player_Has_Back = false;
+        }
+    }
+
+    public void Player_Got_Back()
+    {
+        Player_Has_Back = true;
+    }
+    public void TP_Back()
+    {
+        if (team == 1)
+            transform.SetPositionAndRotation(blue_pos.position, blue_pos.rotation);
+        else
+            transform.SetPositionAndRotation(orange_pos.position, orange_pos.rotation);
+    }
+}
