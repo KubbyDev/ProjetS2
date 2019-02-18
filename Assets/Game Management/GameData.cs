5baf24902ab91943e9ca005cefa3cab02e987bec
Miya <melanie.tcheou@epita.fr>
2019-02-18 11:50:00 +0100

    Game management
    Manque la fin de partie

---
+++ Assets/Game Management/GameData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData : MonoBehaviour
+{
+    public int blueScore;
+    public int orangeScore;
+
+    public GameData()
+    {
+        blueScore = 0;
+        orangeScore = 0;
+    }
+
+    public void BlueTeamScores()
+    {
+        blueScore++;
+    }
+
+    public void OrangeTeamScores()
+    {
+        orangeScore++;
+    }
+
+}
