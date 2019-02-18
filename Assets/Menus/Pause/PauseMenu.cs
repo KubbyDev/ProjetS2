2a04cca005dfcf139aa22648e3ccd48f9e86ecef
Gabriel <gbrljorge@gmail.com>
2019-02-18 11:50:02 +0100

    PauseMenu

---
+++ Assets/Menus/Pause/PauseMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject optionsMenu;
+
+    public void OnResumeClick()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public void OnOptionsClick()
+    {
+        this.gameObject.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+
+    public void OnQuitClick()
+    {
+
+    }
+
+    //Quand on clique sur Back depuis le menu des options
+    public void OnBackClicked()
+    {
+        this.gameObject.SetActive(true);
+        optionsMenu.SetActive(false);
+    }
+}
