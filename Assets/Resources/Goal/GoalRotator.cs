270f18d452e5514194fcdc046f26dd9b91b70b2e
KubbyDev <gbrljorge@gmail.com>
2019-04-25 14:02:38 +0200

    Goal Rotator

---
+++ Assets/Resources/Goal/GoalRotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalRotator : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 20;
+    
+    void Update()
+    {
+        transform.Rotate(Vector3.right, rotationSpeed*Time.deltaTime);
+    }
+}
