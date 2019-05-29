8a37ad24879b9007dd742ae9be0fb737c6b046b2
melanie.tcheou <melanie.tcheou@epita.fr>
2019-05-29 18:54:31 +0200

    ups sinon ca compilait pas

--- Assets/Resources/AI/Skills/Movements.cs
+++ Assets/Resources/AI/Skills/Movements.cs
@@ -113,6 +113,10 @@ public partial class Skills
     /// <summary> Se dirige entre le but et le joueur le plus proche du but (en regardant le but adverse) </summary>
     public void MoveToSupportPosition() => MoveTo((GetNearestOpponentFromAllyGoal().transform.position + AllyGoal().transform.position)/2, false);
 
+    public Vector3 SupportPosition() =>
+        (GetNearestOpponentFromAllyGoal().transform.position + AllyGoal().transform.position) / 2;
+
+
     /// <summary> Se dirige sous les cages pour un arret eventuel </summary>
     public void MoveToDefensivePosition() => MoveTo(AllyGoal().transform.position, false, false, 10f);
     
