21a82ad08b914f145397d5ccfccfd9df2fdc0afb
Gabriel <gbrljorge@gmail.com>
2019-06-03 19:18:54 +0200

    Autre Fix Particule

--- Assets/Resources/Pow-Ups/Back/Back.cs
+++ Assets/Resources/Pow-Ups/Back/Back.cs
@@ -1,10 +1,17 @@
-﻿using UnityEngine;
+﻿using Photon.Pun;
+using UnityEngine;
 
 public class Back : MonoBehaviour
 {    
     public bool Player_Has_Back = false;
 
+    private PhotonView pv;
     [SerializeField] private GameObject theVoidPrefab;
+
+    void Awake()
+    {
+        pv = GetComponent<PhotonView>();
+    }
     
     public void Player_Got_Back()
     {
@@ -15,11 +22,19 @@ public class Back : MonoBehaviour
     {
         if (Player_Has_Back)
         {
-            Instantiate(theVoidPrefab, transform.position, Quaternion.identity);
-                
+            pv.RPC("SpawnBackParticle_RPC", RpcTarget.All, transform.position);
+            
             Spawns.AtRandomUnused(this.gameObject);
             Player_Has_Back = false;
         }
+    }
+
+    [PunRPC]
+    public void SpawnBackParticle_RPC(Vector3 position)
+    {
+        GameObject particle = Instantiate(theVoidPrefab, position, Quaternion.identity);
+        ParticleSystem.MainModule main = particle.GetComponent<ParticleSystem>().main;
+        main.startColor = new ParticleSystem.MinMaxGradient(GetComponent<PlayerInfo>().team.GetMaterial().color);
     }
 }
 
