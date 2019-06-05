b2f1072f1cc0f3083b63e895042d3f8f1b407bfa
Gabriel <gbrljorge@gmail.com>
2019-06-05 12:50:22 +0200

    Escape particle

--- Assets/Resources/Spells/Striker/TeleportBullet.cs
+++ Assets/Resources/Spells/Striker/TeleportBullet.cs
@@ -7,6 +7,7 @@ public class TeleportBullet : MonoBehaviour
     
     private GameObject shooter;    //Reference au joueur
     private float startTime;       //Moment du lancer
+    [SerializeField] private GameObject theVoidParticle;
     
     //True: Cette balle va regarder si elle doit faire une TP (check les collisions et le temps)
     //Si la balle est inactive c'est parce qu'elle appartient a un autre client (seul le client qui lance la balle la calcule)
@@ -31,9 +32,10 @@ public class TeleportBullet : MonoBehaviour
     //Tp le joueur sur la balle
     private void Tp()
     {
+        GetComponent<PhotonView>().RPC("Teleport_RPC", RpcTarget.All, shooter.transform.position, (int) shooter.GetComponent<PlayerInfo>().team);
+        
         //Tp le joueur un peu avant le point de collision pour eviter de passer a travers les murs
         shooter.transform.position = transform.position - direction.normalized*1.0f;
-        GetComponent<PhotonView>().RPC("Destroy_RPC", RpcTarget.All);
     }
 
     //Appellee par Stricker.cs
@@ -52,8 +54,12 @@ public class TeleportBullet : MonoBehaviour
     }
 
     [PunRPC]
-    public void Destroy_RPC()
+    public void Teleport_RPC(Vector3 fromPosition, int team)
     {
+        GameObject particle = Instantiate(theVoidParticle, fromPosition, Quaternion.identity);
+        ParticleSystem.MainModule main = particle.GetComponent<ParticleSystem>().main;
+        main.startColor = new ParticleSystem.MinMaxGradient(((Team) team).GetMaterial().color);
+        
         Destroy(this.gameObject);
     }
 }
